98c2138966daab9e111b68b4558a70dfcdc2061d
James Beem <beem132@gmail.com>
2016-04-02 11:20:10 -0500

    successfully returned city to front end

--- modules/weather/controller/controller.js
+++ modules/weather/controller/controller.js
@@ -1,44 +1,87 @@
 var app = angular.module('Weather',[]);
 
-app.factory('WeatherGetter', function($http){
+app.factory('WeatherGetter', function($http, $q, $rootScope){
+
 	var obj = {};
+	
 	obj.getLoc = function(){
-		return navigator.geolocation;
-	};
-	obj.getKey = function(){
-		$http.get('/key').then(function successCallback(response){
-			console.log(response);
-			return response;
-		}, function errCallback(response){
+		//options for navigator.geolocation.getCurrentPosition
+		var options = {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0
+		};
+		
+		// creating a $q defer to allow the use of a call back once getLoc is returned to the controller
+		var deferred = $q.defer();
+		
+		//call back from httpPost, saves the received city name in to 
+		// the deferred promise for use back in the controller.
+		function successCallback(response){
+			var cityName=response.data;
+			console.log("this is the top end"+cityName);
+			deferred.resolve(cityName);
+		};//end successCallback
+		
+		//error call back from httpPost
+		function errCallback(response){
 			console.log('my api call failed');
-		});
-	};
+		};//end errCallback
+		
+		//post function called to get the city name from google reverse geocoding api
+		//goes through our server first to get google api key.
+		function httpPost(data) {
+			$http.post('/key',data).then(successCallback, errCallback);
+		};
+		
+		//success function called when navigator.geolocation.getCurrentPosition resolves
+		//the pos variable is the geolocation data from the browser.
+		function success(pos){
+			var crd=pos.coords;
+			console.log("logging in getLoc"+crd);
+			var data = {
+				longitude: crd.longitude,
+				latitude: crd.latitude
+			};
+			
+			httpPost(data);//sending the coordinate data to an http post request
+		};//end success function
+		
+		//error function called if navigator.geolocation.getCurrentPosition fails
+		function error(err) {
+			return console.warn('ERROR(' + err.code + '): ' + err.message);
+		};//end error function
+		
+		//checking if there's access to browser location information
+		//if successful navigator.geolocation.getCurrentPosition gets the coordinates
+		//and sets off the chain of call backs, this is the first step.
+		if (navigator.geolocation) {
+			var geoLoc=navigator.geolocation.getCurrentPosition(success, error, options);
+			console.log("logging geoLoc "+geoLoc)
+			
+		}//end navigator.geolocation if statement
+		else { //else statement reports if geolocation is not supported
+			return console.log('Geolocation is not supported for this Browser/OS version yet.');
+		}//end navigator.geolocation else statement
+		
+		//returns an asynchronus promise that can be dealt with when the above
+		//callback chain is done.
+		return deferred.promise;
+	};//end obj.getLoc
+	obj.getWeather = function(city){
+		
+	};//end obj.getWeather
+	
 	return obj;
 });
 
 app.controller('WeatherCtrl',['$scope','$http','WeatherGetter', function($scope,$http, WeatherGetter){
+	WeatherGetter.getLoc().then(function(result){
+		$scope.city = result;
+		console.log("checking that i got the city back "+$scope.city);
+	});
 	
-	WeatherGetter.getLoc();
-	WeatherGetter.getKey();
-	var options = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0
-	};
 	
-	function success(pos){
-		var crd=pos.coords;
-		
-		console.log(pos);
-	};
-	function error(err) {
-		console.warn('ERROR(' + err.code + '): ' + err.message);
-	};
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(success, error, options);
-	}
-	else {
-		console.log('Geolocation is not supported for this Browser/OS version yet.');
-	}
+
 	
 }]);
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,43 @@ var router = express.Router();
 var fs = require('fs');
 var path=require('path');
 var filePath = path.join(__dirname,'keyses.txt');
-
+var qs = require('querystring');
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('weather', { title: 'Express' });
 });
 
-router.get('/key', function(req, res, next){
-	//var key=fs.readFileSync('./config', 'utf8');
-	//res.send('poop');
-	fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err){
-    console.log('received data: ' + data);
-    res.send(data);
-    }else{
-        console.log(err);
-    }
-
-	});
+router.post('/key', function(req, res, next){
+	var googleKey="";
+	var options={};
+	function fileReadCallback(err,data){
+		if (!err){
+			console.log('received data: ' + data);
+			googleKey=data.toString();
+			//within the file read callback defining the file path for the next http request
+			options = {
+				host: 'https://maps.googleapis.com',
+				path:'/maps/api/geocode/json?latlng='+req.body.latitude+','+req.body.longitude+'&key='+googleKey
+			};
+			
+			request(options.host+options.path,function(error, response, body){
+				if(!error && response.statusCode == 200){
+					var city = JSON.parse(body);
+					console.log(city.results[0].address_components[2].long_name);
+					req.mydata=city.results[0].address_components[2].long_name;
+					res.send(req.mydata);
+				}
+			});
+		}else{
+			console.log(err);	
+		}
+	}
+	
+	//getting the api key from file and saving in to googleKey
+	fs.readFile(filePath, {encoding: 'utf-8'},fileReadCallback);
+	console.log("logging some bullshit"+req.mydata);
+	
 });
 
 module.exports = router;
